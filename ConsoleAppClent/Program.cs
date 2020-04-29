e5e1d32f39346840a33f92254d74a27ba077df66
Silvio Jose de Souza <silvio.j.souza@gmail.com>
2020-04-29 14:01:05 -0300

    ajustes

--- ConsoleAppClent/Program.cs
+++ ConsoleAppClent/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.IdentityModel.Clients.ActiveDirectory;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Headers;
@@ -18,8 +19,7 @@ namespace ConsoleAppClent
             client.DefaultRequestHeaders.Accept.Clear();
             client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
             client.DefaultRequestHeaders.Add("Authorization", $"Bearer {token}");
-            //var apiUrl = "http://localhost:61605/api/values"
-            var apiUrlAzure = "https://appdemowebapi.azurewebsites.net/api/values";
+            var apiUrlAzure =  $"{ConfigurationManager.AppSettings["BaseUrl"]}/api/values";
             using (var request = new HttpRequestMessage(HttpMethod.Get, apiUrlAzure))
             {
                 var response = client.SendAsync(request, HttpCompletionOption.ResponseHeadersRead).GetAwaiter().GetResult();
@@ -35,12 +35,10 @@ namespace ConsoleAppClent
         private static string GetToken()
         {
             //Credencial criada para o App que vai chamar a API.
-            string clientID = "a9e24fc4-5bf8-4f5c-9639-ef6fded5b3ce";
-            string clientSecret = "kdA5/?jxo9Cq9IXrX4v]HpcjSRLMe:=3";
-            var cred = new ClientCredential(clientID, clientSecret);
+            var cred = new ClientCredential(ConfigurationManager.AppSettings["ClientId"], ConfigurationManager.AppSettings["ClientSecret"]);
 
             //ClientId da API
-            string resourceUri = "c603531b-1494-445c-8eb8-142dabcd2e5e";
+            string resourceUri = ConfigurationManager.AppSettings["Resource"];
 
             //OAuth2 authority Uri: https://login.microsoftonline.com + TenantId
             string authorityUri = "https://login.microsoftonline.com/94b1e746-0c85-4ddd-99b7-1e6ac22c732b";
